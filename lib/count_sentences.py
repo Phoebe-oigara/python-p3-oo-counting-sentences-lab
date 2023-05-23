efd5a35a1553f40a5b777ce7d1a34a6a76ed8a29
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-23 14:51:15 +0300

    exploring string methods

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,51 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value=None) :
+    self._value = None
+    if value is not None:
+            self.value = value
+
+  @property
+  def value(self):
+        return self._value
+  
+  @value.setter
+  def value (self, new_value):
+     if isinstance (new_value, str):
+        self._value = new_value
+
+     else :
+        print ("The value must be a string.")
+
+  def is_sentence(self):
+     if self._value is not None:
+        return self._value.endswith (".")
+     return False
+
+  def is_question(self):
+      if self._value is not None:
+        return self._value.endswith ("?")
+      return False
+
+  def is_exclamation(self):
+      if self._value is not None:
+        return self._value.endswith ("!")
+      return False
+  
+  def count_sentences(self):
+     if self._value is None:
+            return 0
+     
+     separators = ['.', '!', '?']
+     for separator in separators:
+            self._value = self._value.replace(separator, '.')
+
+     sentences = self._value.split('.')
+     sentences = [sentence for sentence in sentences if sentence.strip() != '']
+     return len(sentences)
+    
+
+  
+     
